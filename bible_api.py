4beea8f31dfff73c807bcbf52369d0d427415a2b
Emmanuel AKinwale <102560108+LopeWale@users.noreply.github.com>
2023-08-12 18:41:36 +0000

    mini initialization of the citation and bible_api

--- bible_api.py
+++ bible_api.py
@@ -1,3 +1,36 @@
 """bible_api.py: 
 Manages interactions with the getbible.net API to fetch Bible verses.
 """
+
+import requests
+import json
+
+class BibleAPI:
+    """Handles interactions with the getbible.net API to fetch Bible verses."""
+
+    def __init__(self, base_url="https://getbible.net/json"):
+        """Initialize the BibleAPI with an optional base URL."""
+        self.base_url = base_url
+
+    def fetch_verse(self, book, chapter, verse):
+        """
+        Fetch a specific Bible verse by book, chapter, and verse number.
+
+        Args:
+            book (str): The name of the book in the Bible.
+            chapter (int): The chapter number.
+            verse (int): The verse number.
+
+        Returns:
+            str: The text of the specified verse, or None if not found.
+        """
+        url = f"{self.base_url}?p={book} {chapter}:{verse}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            json_str = response.text[1:-2]  # Remove leading and trailing parentheses
+            verse_data = json.loads(json_str)
+            return verse_data['book'][0]['chapter'][0]['verse'][0]['verse']
+        else:
+            print(f"Error fetching verse: {response.status_code}")
+            return None
--- citation_recognition.py
+++ citation_recognition.py
@@ -2,3 +2,42 @@
 citation_recognition.py: 
 Contains logic for recognizing Bible verse citations from speech-to-text output.
 """
+import re
+import speech_recognition as sr
+
+class CitationRecognition:
+    """Recognizes Bible verse citations from speech-to-text output."""
+
+    def __init__(self):
+        """Initialize the CitationRecognition with a speech recognizer."""
+        self.recognizer = sr.Recognizer()
+
+    def recognize_speech(self, audio_data):
+        """
+        Convert audio data to text using a speech recognition library.
+
+        Args:
+            audio_data: The audio data to be recognized.
+
+        Returns:
+            str: The recognized text, or None if recognition failed.
+        """
+        try:
+            return self.recognizer.recognize_google(audio_data)
+        except sr.UnknownValueError:
+            print("Speech recognition failed")
+            return None
+
+    def extract_citations(self, text):
+        """
+        Extract Bible verse citations from the given text.
+
+        Args:
+            text (str): The text containing potential citations.
+
+        Returns:
+            list: A list of citations in the format (book, chapter, verse).
+        """
+        # Define a regular expression pattern for citations (e.g., "John 3:16")
+        pattern = r'(\b[A-Za-z\s]+)\s+(\d+):(\d+)\b'
+        return re.findall(pattern, text)
