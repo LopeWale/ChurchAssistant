918349ef68bb8f2ae25733a03b2ae12174d36622
Emmanuel <emmanuel.t.akinwale@gmail.com>
2023-09-10 13:57:30 -0500

    issue persist but will need to fix before running

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from ctypes import CDLL  # For loading the C++ shared library
 from audio_interface import AudioInterface
 from bible_api import BibleAPI
 from citation_recognition import CitationRecognition
 from config import Config
 from display import Display
+from speech_recognition import SpeechRecognition  # Assuming your Python file is named speech_recognition.py
 
 class MainApp:
     def __init__(self):
@@ -11,7 +13,13 @@ class MainApp:
         self.bible_api = BibleAPI()
         self.citation_recognition = CitationRecognition()
         self.display = Display()
-        
+
+        # Initialize the SpeechRecognition class
+        self.speech_recognition = SpeechRecognition()
+
+        # Load the C++ shared library
+        self.cpp_module = CDLL('./performance_module.so')
+
     def run(self):
         # Step 1: Initialize configurations
         self.config.initialize()  # Assuming you have an initialize method in config.py
@@ -26,14 +34,14 @@ class MainApp:
         self.display.show_verse(initial_verse)
         
         # Step 5: Run Citation Recognition
-        audio_data = self.audio_interface.capture_audio()  # Assuming you have a capture_audio method
-        citation = self.citation_recognition.recognize_speech(audio_data)
-        
-        # Step 6: Fetch and Display new verse based on citation
-        book, chapter, verse = citation.split(":")  # Assuming citation is in "Book:Chapter:Verse" format
-        new_verse = self.bible_api.fetch_verse(book, chapter, verse)
-        self.display.show_verse(new_verse)
-        
+        while True:
+            text = self.speech_recognition.recognize_speech()
+            if text:
+                print(f"Recognized text: {text}")
+
+                # Call the process_audio method from the C++ module
+                self.cpp_module.PerformanceModule_process_audio(text.encode('utf-8'))
+
 if __name__ == "__main__":
     app = MainApp()
     app.run()
